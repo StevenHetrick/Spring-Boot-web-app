d1a3fb2de3f8243e96595ff027db17b08f566358
Steven Hetrick <stevenhetrick@msn.com>
2020-01-30 16:54:56 -0500

    second tutorial

--- Prep/src/main/java/com/example/Prep/PrepApplication.java
+++ Prep/src/main/java/com/example/Prep/PrepApplication.java
@@ -1,13 +1,34 @@
 package com.example.Prep;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class PrepApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PrepApplication.class, args);
-	}
+  private static final Logger log = LoggerFactory.getLogger(PrepApplication.class);
 
+  public static void main(String[] args) {
+    SpringApplication.run(PrepApplication.class, args);
+  }
+
+  @Bean
+  public RestTemplate restTemplate(RestTemplateBuilder builder) {
+    return builder.build();
+  }
+
+  @Bean
+  public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+    return args -> {
+      Quote quote = restTemplate.getForObject(
+          "https://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+      log.info(quote.toString());
+    };
+  }
 }
